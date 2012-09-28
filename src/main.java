8835f6c59ec564cfcbcbf83254dac6d67bc5a3dc
Dario Fiorentino <fiorentinodariogmail.com>
2012-09-28 15:29:31 +0200

    2

--- src/main.java
+++ src/main.java
@@ -15,7 +15,7 @@ public class main {
 	  try {
  
 		  /*
-		  URL fXmlFile = new URL("http://xml.betclick.com/odds_en.xml");
+		  URL fXmlFile = new URL("http://feeds.titanbet.com/feed.xml");
 		  InputStream xml = fXmlFile.openStream();
 		  DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		  DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -23,28 +23,21 @@ public class main {
 		  xml.close();
 		   																		*/
 		// /*  
-		File fXmlFile = new File("xml/odds_en.xml");
+		File fXmlFile = new File("xml/feed.xml");
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		Document doc = dBuilder.parse(fXmlFile);
 		doc.getDocumentElement().normalize();
 																				// */
  
-		System.out.println("primo nodo: " + doc.getDocumentElement().getNodeName());
+		//System.out.println("primo nodo: " + doc.getDocumentElement().getNodeName());
 		
-		System.out.println("secondo nodo: " + doc.getDocumentElement().getFirstChild().getNodeName());
 		
-		System.out.println("terzo nodo: " + doc.getDocumentElement().getFirstChild().getFirstChild().getNodeName());
+		//System.out.println("secondo nodo: " + doc.getDocumentElement().getFirstChild().getNodeName());
 		
-		System.out.println("quarto nodo: " + doc.getDocumentElement().getFirstChild().getFirstChild().getFirstChild().getNodeName());
 		
-		System.out.println("quinto nodo: " + doc.getDocumentElement().getFirstChild().getFirstChild().getFirstChild().getFirstChild().getNodeName());
 		
-		System.out.println("sesto nodo: " + doc.getDocumentElement().getFirstChild().getFirstChild().getFirstChild().getFirstChild().getFirstChild().getNodeName());
-		
-		System.out.println("settimo nodo: " + doc.getDocumentElement().getFirstChild().getFirstChild().getFirstChild().getFirstChild().getFirstChild().getFirstChild().getNodeName());
-		
-		NodeList nList = doc.getElementsByTagName("sports");
+		NodeList nList = doc.getElementsByTagName("bookmaker");
 		System.out.println("-----------------------");
  
 		for (int temp = 0; temp < nList.getLength(); temp++) {
@@ -53,10 +46,23 @@ public class main {
 		   if (nNode.getNodeType() == Node.ELEMENT_NODE) {
  
 		      Element eElement = (Element) nNode;
- 
+		      
+		      
+		      
+		      
+		      System.out.println("bookmaker: " + (nList).item(0).getAttributes().getNamedItem("name").getNodeValue());
+		      System.out.println("sport: " + ((NodeList) nNode).item(3).getAttributes().getNamedItem("name").getNodeValue());
+		      System.out.println("categoria: " + ((NodeList) ((NodeList) nNode).item(3)).item(79).getAttributes().getNamedItem("name").getNodeValue());
+		      System.out.println("-----------------------");
+		      
+		      System.out.println("partita: " + ((NodeList) ((NodeList) nNode).item(3)).item(79).getFirstChild().getAttributes().getNamedItem("name").getNodeValue() + "\n");
+		      System.out.println("data: " + ((NodeList) ((NodeList) nNode).item(3)).item(79).getFirstChild().getAttributes().getNamedItem("date").getNodeValue() + "\n");
+		      
 		     
-		      System.out.println("valore settimo nodo: " +  doc.getDocumentElement().getFirstChild().getFirstChild().getFirstChild().getFirstChild().getFirstChild().getFirstChild().getNodeValue());
-		   /*   System.out.println("Last Name : " + getTagValue("lastname", eElement));
+
+		     
+		       
+		   /*  System.out.println("Last Name : " + getTagValue("event", eElement));
 	              System.out.println("Nick Name : " + getTagValue("nickname", eElement));
 		      System.out.println("Salary : " + getTagValue("salary", eElement)); */
  
